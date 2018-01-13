8e52b9d8e94874aa81a3ee726166e5aa30c0b253
iftekhar joy <iftekharjoy1@gmail.com>
2018-01-13 09:13:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(x,y) {
+  return x+y;
+}
+function subtract(x,y) {
+  return x-y;
+}
+function multiply(x,y) {
+  return x*y;
+}
+function divide(x,y) {
+  return x / y;
+}
+console.log(add(1,2));
+console.log(subtract(3,1));
+console.log(multiply(2,2));
+console.log(divide(2,2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
